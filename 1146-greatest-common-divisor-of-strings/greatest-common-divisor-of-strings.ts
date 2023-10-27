b1250374a03fe76ce22ed6cd2ccc69e8bc3c1285
Eric Nguyen <etndevel@gmail.com>
2023-10-27 03:48:25 -0400

    Time: 55 ms (75.43%) | Memory: 44.2 MB (79.40%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.ts
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.ts
@@ -1,22 +1,20 @@
 function gcdOfStrings(str1: string, str2: string): string {
     while ((str1.indexOf(str2) >= 0 || str2.indexOf(str1) >= 0) && str1.length != str2.length) {
-        while (str1.indexOf(str2) >= 0 && str1.length != str2.length) {
+        if (str1.indexOf(str2) >= 0 && str1.length != str2.length) {
             const idx = str1.indexOf(str2);
             if (idx % str2.length != 0) {
                 return "";
             }
-            str1 = str1.substring(0, idx) + str1.substring(idx + str2.length, str1.length);
+            str1 = removeString(str1, str2);
         }
-        while (str2.indexOf(str1) >= 0 && str1.length != str2.length) {
+        if (str2.indexOf(str1) >= 0 && str1.length != str2.length) {
             const idx = str2.indexOf(str1);
             if (idx % str1.length != 0) {
                 return "";
             }
-            str2 = str2.substring(0, idx) + str2.substring(idx + str1.length, str2.length);
+            str2 = removeString(str2, str1);
         }
     }
-    console.log(str1);
-    console.log(str2);
     if (str1 == str2) {
         return str1;
     }
@@ -28,6 +26,11 @@ function gcdOfStrings(str1: string, str2: string): string {
     }
     return "";
 };
+
+function removeString(str1: string, str2: string): string {
+    const idx = str1.indexOf(str2);
+    return str1.substring(0, idx) + str1.substring(idx + str2.length, str1.length);
+}
 
 function gcd(str: string): string {
     let arr = [str[0]];
