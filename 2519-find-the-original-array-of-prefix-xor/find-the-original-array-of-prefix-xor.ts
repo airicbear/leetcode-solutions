7fe95e2004296380a9811f8d079c6df889bf0c29
Eric Nguyen <etndevel@gmail.com>
2023-10-31 18:09:11 -0400

    Time: 164 ms (59.26%) | Memory: 67.5 MB (51.85%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.ts
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.ts
@@ -1,10 +1,8 @@
 function findArray(pref: number[]): number[] {
     const arr = [pref[0]];
-    let val = arr[0];
     
     for (let i = 1; i < pref.length; i++) {
-        arr.push(val ^ pref[i]);
-        val = pref[i];
+        arr.push(pref[i - 1] ^ pref[i]);
     }
     
     return arr;
