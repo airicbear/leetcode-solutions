1338d987cf397049c91f031ba7f74b2a04aab14b
Eric Nguyen <etndevel@gmail.com>
2023-11-06 18:08:37 -0500

    Time: 331 ms (5.37%) | Memory: 52.1 MB (5.09%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,24 +1,14 @@
 function reverseWords(s: string): string {
     const arr = [];
     let word = "";
-    let i = s.length - 1;
-    while (i >= 0) {
-        if (s[i] != ' ') {
-            let j = i;
-            let length = 0;
-            while (j >= 0 && s[j] != ' ') {
-                j--;
-                i--;
-                length++;
-            }
-            j = 0;
-            while (j < length) {
-                word += s[j + i + 1];
-                j++;
-            }
-            arr.push(word);
+    for (let i = 0; i < s.length + 1; i++) {
+        if (i < s.length && s[i] != ' ') {
+            word += s[i];
         } else {
-            i--;
+            if (word != '') {
+                arr.unshift(word);
+                console.log(arr);
+            }
             word = "";
         }
     }
