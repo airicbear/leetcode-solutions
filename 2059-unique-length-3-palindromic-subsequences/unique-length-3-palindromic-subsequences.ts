04923ddcb2884d190fd6e67861014ce84590fa95
Eric Nguyen <etndevel@gmail.com>
2023-11-14 21:20:11 -0500

    Time: 351 ms (87.50%) | Memory: 50.1 MB (87.50%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.ts
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.ts
@@ -1,28 +1,18 @@
 function countPalindromicSubsequence(s: string): number {
-    const map = {};
-    const max = 676; // max 3-palindromic subsequences = 26^2
+    const letters = Array.from(new Set(s.split('')));
     let sum = 0;
-    for (let i = 0; i < s.length - 1; i++) {
-        if (map[s[i]] == null) {
-            map[s[i]] = new Set([]);
+
+    for (const letter of letters) {
+        const i = s.indexOf(letter);
+        const j = s.lastIndexOf(letter);
+        const between = new Set([]);
+
+        for (let k = i + 1; k < j; k++) {
+            between.add(s[k]);
         }
-        if (map[s[i]].has(s[i+1])) continue;
-        const tmp = new Set([]);
-        for (let j = i + 1; j < s.length; j++) {
-            if (tmp.has(s[j])) continue;
-            tmp.add(s[j]);
-            if (!map[s[i]].has(s[j])) {
-                for (let k = j + 1; k < s.length; k++) {
-                    if (s[i] == s[k] && !map[s[i]].has(s[j])) {
-                        map[s[i]].add(s[j]);
-                        sum++;
-                    }
-                    if (sum >= max) {
-                        return max;
-                    }
-                }
-            }
-        }
+
+        sum += between.size;
     }
+
     return sum;
 };
