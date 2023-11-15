517bba2fa4bf62ff8d974610d31b1a4b896c3d21
Eric Nguyen <etndevel@gmail.com>
2023-11-14 21:36:22 -0500

    Time: 305 ms (95.00%) | Memory: 46.4 MB (97.50%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.ts
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.ts
@@ -1,16 +1,25 @@
 function countPalindromicSubsequence(s: string): number {
-    const letters = Array.from(new Set(s.split('')));
+    const first = Array(26).fill(-1);
+    const last = Array(26).fill(-1);
+
+    for (let i = 0; i < s.length; i++) {
+        const curr = s.charCodeAt(i) - 'a'.charCodeAt(0);
+        if (first[curr] == -1) {
+            first[curr] = i;
+        }
+        last[curr] = i;
+    }
+
     let sum = 0;
-
-    for (const letter of letters) {
-        const i = s.indexOf(letter);
-        const j = s.lastIndexOf(letter);
-        const between = new Set([]);
-
-        for (let k = i + 1; k < j; k++) {
-            between.add(s[k]);
+    for (let i = 0; i < 26; i++) {
+        if (first[i] == -1) {
+            continue;
         }
 
+        const between = new Set();
+        for (let j = first[i] + 1; j < last[i]; j++) {
+            between.add(s.charAt(j));
+        }
         sum += between.size;
     }
 
